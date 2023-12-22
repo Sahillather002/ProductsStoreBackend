c4e815d17d334e38431e3093175a786ff6d65ff5
SahilGrip <sahillather002@gmail.com>
2023-12-22 14:15:31 +0530

    Product Component Added

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SharedImage extends Schema.Component {
+  collectionName: 'components_shared_images';
+  info: {
+    displayName: 'image';
+  };
+  attributes: {
+    mobileUrl: Attribute.Media;
+    desktopUrl: Attribute.Media;
+    label: Attribute.String;
+    link: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'shared.image': SharedImage;
+    }
+  }
 }
